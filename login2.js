471fd9d2141299d1edf793384815461aaa430375
bed-deb <julus06@o2.pl>
2026-06-23 20:24:55 +0200

    Change correct password in login function
    Updated the correct password in the login function.

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ function login() {
         var password = document.getElementById('password').value;
 
         // Tablica przechowująca poprawne hasła
-        var correctPasswords = ["03.06"]; // Możesz dodać więcej haseł tutaj
+        var correctPasswords = ["26.06"]; // Możesz dodać więcej haseł tutaj
 
         // Sprawdzenie, czy wprowadzone hasło znajduje się w tablicy
         if (password === "") {
